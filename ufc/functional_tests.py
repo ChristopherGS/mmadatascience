fb291590627b14dd7e38d8dd668ced59cf0f4931
ChristopherGS <christopher.samiullah@gmail.com>
2015-04-05 10:38:56 +0800

    Merge branch 'master' of https://github.com/ChristopherGS/schematicBuilder Merge branch 'master' of https://github.com/ChristopherGS/schematicBuilder

--- ufc/functional_tests.py
+++ ufc/functional_tests.py
@@ -4,5 +4,7 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+assert '' in browser.title
+
+if __name__ == '__main__'
 
--- ufc/tests.py
+++ ufc/tests.py
@@ -8,11 +8,18 @@ from .models import Fighter, SearchResult
 
 
 class WebTests(TestCase):
-	def test_check_browser(self):
-		browser = webdriver.Firefox()
-		browser.get('http://localhost:8000')
 
-		assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_check_browser(self):
+
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Django', self.browser.title)
 
 
 #Beautiful soup should be available from index.html
