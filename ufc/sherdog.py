0a4c6b9f0c87d795545818ebd1f0600d288669ce
ChristopherGS <christopher.samiullah@gmail.com>
2015-04-19 10:43:12 +0800

    scraping fighter record

--- ufc/sherdog.py
+++ ufc/sherdog.py
@@ -2,6 +2,7 @@ import urllib2
 import requests
 import mechanize
 import html5lib
+import datetime
 
 from bs4 import BeautifulSoup
 
@@ -62,13 +63,174 @@ class Scraper(object):
 		source = urllib2.urlopen(aurl).read()
 		soup = BeautifulSoup(source)
 
-		print soup.body
+		#print soup.body
 		fighter_name = soup.find_all("span", class_="fn")
+		fighter_namey = soup.find('h1', {'itemprop': 'name'}).span.contents[0]
 		print fighter_name
 
-		#print 'status code', soup.status_code
-		#print 'headers', soup.headers
-		#print 'cookies', requests.utils.dict_from_cookiejar(s.cookies)
+		print fighter_namey
+
+
+		try:
+			birth_date = soup.find('span', {'itemprop': 'birthDate'}).contents[0]
+		except AttributeError:
+			birth_date = None
+		else:
+			if birth_date == 'N/A':
+			    birth_date = None
+			else:
+			    birth_date = datetime.datetime.strptime(birth_date, '%Y-%m-%d')
+			    birth_date = birth_date.isoformat()
+
+		print 'birthday', birth_date
+
+
+			# get the fighter's locality
+		try:
+			locality = soup.find('span', {'itemprop': 'addressLocality'}).contents[0]
+		except AttributeError:
+			locality = None
+
+			# get the fighter's locality
+		try:
+			nationality = soup.find('strong', {'itemprop': 'nationality'}).contents[0]
+		except AttributeError:
+			nationality = None
+
+			# get the fighter's height in CM
+		try:
+			height_cm = soup.find('span', {'class': 'item height'}).contents[-1].lstrip().rstrip().replace(' cm', '')
+		except AttributeError:
+			height_cm = None
+
+			# get the fighter's weight in KG
+		try:
+			weight_kg = soup.find('span', {'class': 'item weight'}).contents[-1].lstrip().rstrip().replace(' kg', '')
+		except AttributeError:
+			weight_kg = None
+
+			# get the fighter's camp/team
+		try:
+			camp_team = soup.find('h5', {'class': 'item association'}).strong.span.a.span.contents[0]
+		except AttributeError:
+			camp_team = None
+
+		wld = {}
+		wld['wins'] = 0
+		wld['losses'] = 0
+		wld['draws'] = 0
+		wlds = soup.findAll('span', {'class': 'result'})
+		for x in wlds:
+			wld[x.contents[0].lower()] = x.findNextSibling('span').contents[0]
+
+		last_fight = soup.find('span', {'class': 'sub_line'}).contents[0]
+		last_fight = datetime.datetime.strptime(last_fight, '%b / %d / %Y')
+		last_fight = last_fight.isoformat()
+
+			# build a dict with the scraped data and return it
+		result = {
+			'name': fighter_namey,
+			'birth_date': birth_date,
+			'locality': locality,
+			'nationality': nationality,
+			'height_cm': height_cm,
+			'weight_kg': weight_kg,
+			'camp_team': camp_team,
+			#'id': fighter_id,
+			'wins': wld['wins'],
+			'losses': wld['losses'],
+			'draws': wld['draws'],
+			'last_fight': last_fight,
+			}
+
+
+		record = soup.find('span', {'class': 'final_result'}).contents[0]
+		# the record table is the second table on the page
+		
+		records = {}
+		records = soup.findAll('table')[1].findAll('tr')
+
+		#for td in soup.select('table(1) > tr > td:nth-of-type(1)'):
+
+		history = {}
+		history['win_loss'] = []
+		history['opponent'] = []
+		history['event'] = []
+		history['date'] = []
+		history['method_general'] = []
+		history['method_specific'] = []
+		history['referee'] = []
+		history['_round'] = []
+		history['time'] = []
+
+		def getEvent(eventString):
+			dateStrings = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+			for x in dateStrings:
+				index = eventString.find(x)
+				if (index == -1):
+					pass
+				else:
+					return eventString[:index]
+
+		def getDate(eventString):
+			dateStrings = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+			for x in dateStrings:
+				index = eventString.find(x)
+				if (index == -1):
+					pass
+				else:
+					return eventString[index:]
+
+		def get_general_method(eventString):
+				index = eventString.find('(')
+				if (index == -1):
+					pass
+				else:
+					return eventString[:index]
+
+		def get_specific_method(eventString):
+			index = eventString.find('(')
+			end_index = eventString.find(')')
+			if (index == -1):
+				pass
+			else:
+				return eventString[index+1:end_index]
+
+		def get_ref(eventString):
+			index = eventString.find(')')
+			if (index == -1):
+				pass
+			else:
+				return eventString[index+1:]
+
+
+		for row in records:
+			cells = row.find_all('td')
+			win_loss = [cells[0].get_text()] 
+			opponent = cells[1].get_text()
+			event = getEvent(cells[2].get_text())
+			date = getDate(cells[2].get_text()) #print event #need to work magic here to separate date
+			method_general = get_general_method(cells[3].get_text())
+			method_specific = get_specific_method(cells[3].get_text())
+			referee = get_ref(cells[3].get_text())
+			_round = cells[4].get_text()
+			time = cells[5].get_text()
+
+			history['win_loss'].append(win_loss)
+			history['opponent'].append(opponent)
+			history['event'].append(event)
+			history['date'].append(date) 
+			history['method_general'].append(method_general)
+			history['method_specific'].append(method_specific)
+			history['referee'].append(referee)
+			history['_round'].append(_round)
+			history['time'].append(time)
+
+		
+		print history
+
+		print result
+
 		return #result.text
 
 	def scrape_fighter(self, name, fighter_id):
@@ -81,23 +243,6 @@ class Scraper(object):
 
 		# fetch the required url and parse it
 		url_content = self.fetch_url('/fighter/%s-%s' % (name, fighter_id))
-		#soup = BeautifulSoup(url_content, 'html5lib')
-
-		#print soup.body
-
-		#fighter_name = soup.find_all("span", class_="fn")
-
-		#print fighter_name
-
-
-
-	def search(cls, query):
-		pass
-#		query = urllib2.quote(query.lower())
-#		dom = Sherdog.fetch_and_parse_url(cls._search_url_path % query)
-#		table = dom.find('table', {'class': 'fightfinder_result'})
-#		urls = [a['href'] for a in table.findAll('a')]
-#		return map(cls, filter(_FIGHTER_URL_RE.match, urls))
 
 
 if __name__ == "__main__":
