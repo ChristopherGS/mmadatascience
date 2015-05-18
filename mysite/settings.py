513f6b51b64e1235cf1debd53917b65f8bab4ce8
ChristopherGS <christopher.samiullah@gmail.com>
2015-05-18 09:49:50 +0800

    bring in env variables for prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,19 +14,15 @@ import settings
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # For Heroku
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mjk37d#e!&_2g9e%^^*juvg_(#k&h(2kvf@^5cgptgv+p@6jrn'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,9 +44,6 @@ TEMPLATES = [
         },
     },
 ]
-
-
-
 
 # Application definition
 
@@ -78,20 +71,15 @@ ROOT_URLCONF = 'mysite.urls'
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
-
-
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'yournewdb',
         'USER': 'christophersamiullah',
-        'PASSWORD': '2274',
+        'PASSWORD': os.environ['DB_PW'],
         'HOST': 'localhost',
         'PORT': '5432',
 
@@ -111,16 +99,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-
-
 # heroku
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -128,11 +113,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 
 # setup logging
 
